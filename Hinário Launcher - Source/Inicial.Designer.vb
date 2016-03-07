fec558c86e1576124b99f5c40a9238b4ab34e607
GabrielAugustoTI <GabrielAugustoTI@outlook.com>
2016-03-07 20:59:21 -0300

    Upload do Launcher do Hinário

---
+++ Hinário Launcher - Source/Inicial.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Inicial
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Inicial))
+        Me.NumeroHino = New System.Windows.Forms.TextBox()
+        Me.Abrir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'NumeroHino
+        '
+        Me.NumeroHino.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.NumeroHino.Location = New System.Drawing.Point(204, 449)
+        Me.NumeroHino.Name = "NumeroHino"
+        Me.NumeroHino.Size = New System.Drawing.Size(52, 32)
+        Me.NumeroHino.TabIndex = 0
+        Me.NumeroHino.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Abrir
+        '
+        Me.Abrir.Location = New System.Drawing.Point(204, 482)
+        Me.Abrir.Name = "Abrir"
+        Me.Abrir.Size = New System.Drawing.Size(52, 22)
+        Me.Abrir.TabIndex = 1
+        Me.Abrir.Text = "Abrir"
+        Me.Abrir.UseVisualStyleBackColor = True
+        '
+        'Inicial
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(455, 560)
+        Me.Controls.Add(Me.Abrir)
+        Me.Controls.Add(Me.NumeroHino)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Inicial"
+        Me.Text = "Hinário Adventista do Sétimo Dia"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NumeroHino As System.Windows.Forms.TextBox
+    Friend WithEvents Abrir As System.Windows.Forms.Button
+
+End Class
---
+++ Hinário Launcher - Source/Inicial.vb
@@ -0,0 +1,46 @@
+﻿Public Class Inicial
+    Private Sub Settings_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+    Private Sub Inicial_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        NumeroHino.MaxLength = 3
+    End Sub
+
+    Private Sub Abrir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Abrir.Click
+        If String.IsNullOrWhiteSpace(NumeroHino.Text) Then
+        Else
+            Dim hinoformatado As String
+            Dim hino As Integer
+            hino = NumeroHino.Text
+            hinoformatado = hino.ToString("D3")
+            If My.Computer.FileSystem.FileExists(hinoformatado + ".exe") Then
+                Process.Start(hinoformatado + ".exe")
+            End If
+            NumeroHino.Text = ""
+        End If
+        NumeroHino.Focus()
+    End Sub
+    Private Sub NumeroHino_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles NumeroHino.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If String.IsNullOrWhiteSpace(NumeroHino.Text) Then
+            Else
+                Dim hinoformatado As String
+                Dim hino As Integer
+                hino = NumeroHino.Text
+                hinoformatado = hino.ToString("D3")
+                If My.Computer.FileSystem.FileExists(hinoformatado + ".exe") Then
+                    Process.Start(hinoformatado + ".exe")
+                ElseIf My.Computer.FileSystem.FileExists(hinoformatado + ".pps") Then
+                    Process.Start(hinoformatado + ".pps")
+                ElseIf My.Computer.FileSystem.FileExists(hinoformatado + ".ppsx") Then
+                    Process.Start(hinoformatado + ".ppsx")
+                End If
+                NumeroHino.Text = ""
+            End If
+            e.SuppressKeyPress = True
+        End If
+    End Sub
+End Class
+
+
+
